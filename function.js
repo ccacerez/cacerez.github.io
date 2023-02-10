e444c217df0737632c67369196d03b4c418b5b0b
Cacerez Carlos <122688700+ccacerez@users.noreply.github.com>
2023-02-10 13:51:02 -0300

    initial commit

---
+++ function.js
@@ -0,0 +1,8 @@
+
+  document.getElementById('boton').onclick = function() {
+    console.log("Más información");
+    document.getElementById("demo").innerHTML = "telegram 3751-574502 whatsapp 3751-574502 e-mail- williams777111@gmail.com";
+}  
+  
+     
+// practicando mi primer script/
